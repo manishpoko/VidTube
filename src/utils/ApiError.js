4c34c02dbe48b4bf9689ce966a4829a3e4493764
manishpoko <manishpokhrel313@gmail.com>
2025-08-09 00:44:31 +0530

    utils description

--- src/utils/ApiError.js
+++ src/utils/ApiError.js
@@ -8,7 +8,7 @@ class ApiError extends Error {
         //material inside the constructor:
         super(message) //this officially passes the message up to the original "Error" class, so that it behaves like a standard error. !!!MANDATORY LINE WHEN USING "extends"!!!
         this.statusCode = statusCode 
-        this.data =  //conventional property (placeholder)
+        this.data = null //conventional property (placeholder)
         this.message = message
         this.success = false //apparently this is imp, and makes frontend folks' lives easier
         this.errors = errors
